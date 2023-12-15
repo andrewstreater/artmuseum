55a00ecca3053124ced85c2c0c073fef328822fd
Hproud <havin.becca.93@gmail.com>
2023-12-14 21:46:47 -0600

    phase-1 complete

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
+{
+  path:'/',
+  element:
+   <>
+  <h2>Harvard Art Museum</h2>
+  <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+  </>
+},
     {
       path: '*',
       element: <h2>Page Not Found</h2>

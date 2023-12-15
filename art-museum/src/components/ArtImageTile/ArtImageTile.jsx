f1cdcb342c56a09085bf108cfc3576d62b98ac7c
Andrew Streater <streater.andrew@gmail.com>
2023-12-14 21:57:19 -0800

    end of class

--- art-museum/src/components/ArtImageTile/ArtImageTile.jsx
+++ art-museum/src/components/ArtImageTile/ArtImageTile.jsx
@@ -3,12 +3,21 @@ import { useParams } from "react-router-dom"
 
 
 export default function ArtImageTile({art}) {
-const artId = useParams();
+const { artId, galleryId} = useParams();
 const foundGallery = art.find((record) => {
-
+  record.id === +galleryId
 });
 
+const something = foundGallery.objects
+
+const artArr = []
+
+for (let i = 0; i < something.length; i++) {
+  let image = something[i].images
+
+}
+
   return (
-    <div>ArtImageTile</div>
+    <div>hi</div>
   )
 }
